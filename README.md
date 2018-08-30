2701264932996b5dc2b3c2aabd47eaa13b55e85b
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-08-30 09:44:33 +0530

    [ADD]

---
+++ README.md
@@ -0,0 +1,29 @@
+### Easy Install:
+#### Clone and run as a seperate application
+```shell
+    $ git clone https://github.com/saikatharryc/pm2-prometheus-exporter.git
+    $ npm install
+    $ pm2 start exporter.js --name pm2-metrics
+```
+
+
+
+#### Open your browser : 
+```shell
+http://localhost:9209/metrics
+```
+
+#### Prometheus config:
+in `prometheus.yaml`
+ inside `scrape_configs` add this block:
+
+    - job_name: pm2-metrics
+    scrape_interval: 10s
+    scrape_timeout: 10s
+    metrics_path: /metrics
+    scheme: http
+    static_configs:
+      - targets:
+          - localhost:9209
+
+##### *change host name from `localhost` on basics where you are hosting.
---
+++ exporter.js
@@ -0,0 +1,107 @@
+const http = require("http");
+const prom = require("prom-client");
+const pm2 = require("pm2");
+const argv = process.argv;
+
+const prefix = "pm2";
+const labels = ["name", "instance"];
+const map = [
+  ["up", "Is the process running"],
+  ["cpu", "Process cpu usage"],
+  ["memory", "Process memory usage"],
+  ["uptime", "Process uptime"],
+  ["instances", "Process instances"],
+  ["restarts", "Process restarts"]
+];
+
+function pm2c(cmd, args = []) {
+  return new Promise((resolve, reject) => {
+    pm2[cmd](args, (err, resp) => {
+      if (err) return reject(err);
+      resolve(resp);
+    });
+  });
+}
+
+function metrics() {
+  let pm = {};
+  prom.register.clear();
+  map.forEach(m => {
+    pm[m[0]] = new prom.Gauge(prefix + "_" + m[0], m[1], labels);
+  });
+  return pm2c("list").then(list => {
+    list.forEach(p => {
+      let conf = {
+        name: p.name,
+        instance: p.pm2_env.NODE_APP_INSTANCE
+      };
+
+      let values = {
+        up: p.pm2_env.status === "online" ? 1 : 0,
+        cpu: p.monit.cpu,
+        memory: p.monit.memory,
+        uptime: Math.round((Date.now() - p.pm2_env.pm_uptime) / 1000),
+        instances: p.pm2_env.instances || 1,
+        restarts: p.pm2_env.restart_time
+      };
+
+      var names = Object.keys(p.pm2_env.axm_monitor);
+
+      for (let index = 0; index < names.length; index++) {
+        const name = names[index];
+
+        try {
+          let value;
+          if (name === "Loop delay") {
+            value = parseFloat(
+              p.pm2_env.axm_monitor[name].value.match(/^[\d.]+/)[0]
+            );
+          } else {
+            value = p.pm2_env.axm_monitor[name].value;
+          }
+
+          var metricName =
+            prefix + "_" + name.replace(/[^A-Z0-9]+/gi, "_").toLowerCase();
+
+          if (!pm[metricName]) {
+            pm[metricName] = new prom.Gauge(metricName, name, labels);
+          }
+
+          values[metricName] = value;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      Object.keys(values).forEach(k => {
+        if (values[k] === null) return null;
+
+        pm[k].set(conf, values[k]);
+      });
+    });
+
+    return prom.register.metrics();
+  });
+}
+
+function exporter() {
+  const server = http.createServer((req, res) => {
+    switch (req.url) {
+      case "/":
+        return res.end(
+          '<html>PM2 metrics: <a href="/metrics">/metrics</a></html>'
+        );
+      case "/metrics":
+        return metrics().then(data => res.end(data));
+      default:
+        return res.end("404");
+    }
+  });
+
+  const port = Number(argv[1]) || 9209;
+
+  server.listen(port);
+  console.log("pm2-prometheus-exporter listening at %s", port);
+}
+
+exporter();

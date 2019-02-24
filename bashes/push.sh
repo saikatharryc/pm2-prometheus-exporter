5cf2026401135093099612927ea8869d7732929a
saikat <saikatchakrabortty2@gmail.com>
2019-02-24 14:15:18 +0530

    check on node version 10 & if tag exist dont count it to publish again

--- bashes/push.sh
+++ bashes/push.sh
@@ -3,6 +3,8 @@ git config --global user.email "saikatchakrabortty2@gmail.com"
 git config --global user.name "Travis CI"
 git add .
 git commit -a -m "${COMMIT_MESSAGE}" -m '[ci skip]'
+LATEST_TAG="$(git fetch origin && git tag | tail -1)"
+if [[ LATEST_TAG -eq  ${COMMIT_MESSAGE} ]]; then exit 0;
 git tag -a "${COMMIT_MESSAGE}" -m "${LAST_COMMIT_MESSAGE}" -m "" -m "[ci skip]"
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/saikatharryc/pm2-prometheus-exporter.git

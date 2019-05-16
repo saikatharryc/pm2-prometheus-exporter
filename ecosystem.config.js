3c204471a9a51ae68e3176a27c6b821d742d54ed
Aruldd <aruleeswaran@gmail.com>
2019-05-16 07:57:52 +0000

    Added PM2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: 'PM2 Exporter',
+    script: 'exporter.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
